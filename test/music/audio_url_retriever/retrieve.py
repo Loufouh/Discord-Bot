203b4edb7038a7cc29e63bd5b4091bec844035bd
Louis700 <louis.masson.contact@gmail.com>
2022-08-05 12:12:23 +0000

    :white_check_mark: Add audio_url_retriever test skipping (to avoid getting blacklisted by Youtube servers)

--- test/music/audio_url_retriever/retrieve.py
+++ test/music/audio_url_retriever/retrieve.py
@@ -1,7 +1,12 @@
+from config import config
+
 import unittest
+
 from music.audio_url_retriever import get_retriever
 
 class TestAudioURLRetriever_retrieve(unittest.TestCase):
+    @unittest.skipIf(config['skipRequestTests'],
+                    'Avoid spamming Youtube servers')
     def test(self):
         retriever = get_retriever()
 
