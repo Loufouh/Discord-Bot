506d2ffdc7b0e0d12090be4b0755427eb8a7ba0d
Louis700 <louis.masson.contact@gmail.com>
2022-08-06 15:07:37 +0000

    :white_check_mark: Improve audio_get_retriever skipping message

--- test/music/audio_url_retriever/retrieve.py
+++ test/music/audio_url_retriever/retrieve.py
@@ -6,7 +6,7 @@ from music.audio_url_retriever import get_retriever
 
 class TestAudioURLRetriever_retrieve(unittest.TestCase):
     @unittest.skipIf(config['skipRequestTests'],
-                    'Avoid spamming Youtube servers')
+                    'To avoid spamming Youtube servers')
     def test(self):
         youtubeURL = 'https://youtu.be/paveeT8QPXA'
         url = get_retriever().retrieve(youtubeURL)
