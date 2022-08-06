0055cdd4756944334c88b1d6260e4565c7e071af
Louis700 <louis.masson.contact@gmail.com>
2022-08-06 15:35:15 +0000

    :white_check_mark: Add test for context_dummy._connect_author

---
+++ test/commands/dummies/test/context/_connect_author.py
@@ -0,0 +1,13 @@
+import unittest
+
+from test.commands.dummies.context import Context_dummy
+from test.commands.dummies.voice_state import VoiceState_dummy
+
+class TestContext__connect_author(unittest.IsolatedAsyncioTestCase):
+    async def test(self):
+        ctx = Context_dummy()
+
+        await ctx._connect_author()
+
+        self.assertIsInstance(ctx.author.voice, VoiceState_dummy)
+
