6ded5b210086d99abe817197f730233f1804a400
Louis700 <louis.masson.contact@gmail.com>
2022-08-06 15:19:25 +0000

    :white_check_mark: Add test for default context dummy

---
+++ test/commands/dummies/test/context/default.py
@@ -0,0 +1,14 @@
+import unittest
+
+from test.commands.dummies.context import Context_dummy
+from test.commands.dummies.author import Author_dummy
+
+class TestContext_default(unittest.TestCase):
+    def setUp(self):
+        self.context = Context_dummy()
+
+    def test(self):
+        self.assertIsInstance(self.context.author, Author_dummy)
+        self.assertIsNone(self.context.voice_client)
+        self.assertEqual(self.context.sent, '')
+
