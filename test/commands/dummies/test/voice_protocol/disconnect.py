dfe89de33f6e04c6d8870a48481f5d881a23b025
Louis700 <louis.masson.contact@gmail.com>
2022-08-07 08:00:44 +0000

    :test_tube: Add failing test for VoiceProtocol_dummy.disconnect

---
+++ test/commands/dummies/test/voice_protocol/disconnect.py
@@ -0,0 +1,19 @@
+import unittest
+
+from test.commands.dummies.context import Context_dummy
+
+
+class TestVoiceProtocol_disconnect(unittest.IsolatedAsyncioTestCase):
+    async def test(self):
+        ctx = Context_dummy()
+
+        await ctx._connect_author()
+        await ctx._connect()
+
+        await ctx.voice_client.disconnect()
+
+        self.assertIsNone(ctx.voice_client)
+        self.assertIsNotNone(ctx.author.voice)
+        
+        self.assertTrue(ctx.author.voice.channel._isConnected)
+
