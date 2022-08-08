35d52d29a0775b43ab67140cd18a21c160d34c8e
Louis700 <louis.masson.contact@gmail.com>
2022-08-08 07:49:56 +0000

    :recycle: Create test.commands.dummies.ffmpeg_handler

---
+++ test/commands/dummies/ffmpeg_handler.py
@@ -0,0 +1,6 @@
+import discord
+
+class FFmpegHandler_dummy:
+    def create_audio_from_url(self, url):
+        return discord.AudioSource()
+
---
+++ test/commands/dummies/test/ffmpeg_handler/create_audio_from_url.py
@@ -0,0 +1,12 @@
+import unittest
+import discord
+
+from test.commands.dummies.ffmpeg_handler import FFmpegHandler_dummy
+
+class TestFFmpegHandler_create_from_url(unittest.TestCase):
+    def test(self):
+        handler = FFmpegHandler_dummy()
+
+        audio = handler.create_audio_from_url('random_fake_url')
+
+        self.assertIsInstance(audio, discord.AudioSource)
