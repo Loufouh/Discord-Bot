44b48e87c9bd4f855080b52e7a2924af9fda61b5
Louis700 <louis.masson.contact@gmail.com>
2022-08-10 10:29:28 +0000

    :recycle: Refactoring

--- test/commands/test_play.py
+++ test/commands/test_play.py
@@ -1,15 +1,13 @@
 import unittest
 
 from commands.play import _play
+from commands.objects.play import get_command
 from dummies.context import Context_dummy
-
-from commands.objects.play import get_command as get_play_command
 
 class TestPlay(unittest.IsolatedAsyncioTestCase):
     async def test(self):
         ctx = Context_dummy()
-
         await _play(ctx, 'fake_link')
 
-        self.assertTrue(get_play_command()._hasBeenExecuted)
+        self.assertTrue(get_command()._hasBeenExecuted)
 
--- test/dummies/commands/objects/play/test_default.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from dummies.commands.objects.play import PlayCommand_dummy
-
-class TestPlayCommand_default(unittest.TestCase):
-    def test(self):
-        command = PlayCommand_dummy()
-        self.assertFalse(command._hasBeenExecuted)
-
