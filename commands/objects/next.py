bf31bb6f41ad6adc5183dc00b1e0cc5bfa6f0666
Louis700 <louis.masson.contact@gmail.com>
2022-08-17 12:11:22 +0000

    :speech_balloon: Add messages responses for commands.objects.next

--- commands/objects/next.py
+++ commands/objects/next.py
@@ -1,8 +1,34 @@
+from commands.exceptions.exceptions import AuthorNotConnectedException, NotConnectedException, NotPlayingException
+
 from music.queue_player import get_queue_player
+
+from message_sender import MessageSender
 
 class NextCommand:
     def __init__(self):
         self.player = get_queue_player()
 
     async def execute(self, ctx):
+        self.messageSender = MessageSender(ctx)
+
+        try:
+            await self.try_to_execute(ctx)
+        except AuthorNotConnectedException:
+            await self.messageSender.send('Tu dois être connecté à un salon audio pour ça')
+        except NotConnectedException:
+            await self.messageSender.send('Je suis pas connecté au salon')
+        except NotPlayingException:
+            await self.messageSender.send('Je joue rien pour l\'instant')
+
+    async def try_to_execute(self, ctx):
+        if ctx.author.voice is None:
+            raise AuthorNotConnectedException()
+        if ctx.voice_client is None:
+            raise NotConnectedException()
+        if not ctx.voice_client.is_playing():
+            raise NotPlayingException()
+
         self.player.next()
+
+        await self.messageSender.send('Ça marche')
+
--- test/commands/objects/next/test_execute.py
+++ test/commands/objects/next/test_execute.py
@@ -3,19 +3,55 @@ import discord
 
 from commands.objects.next import NextCommand
 
+from music.queue import Queue
+
 from dummies.context import Context_dummy
 from dummies.queue_player import QueuePlayer_dummy
 
 class TestNextCommand_execute(unittest.IsolatedAsyncioTestCase):
-    def setUp(self):
+    async def asyncSetUp(self):
         self.ctx = Context_dummy()
         self.command = NextCommand()
+        self.queue = Queue()
         self.player = QueuePlayer_dummy()
 
+        self.queue.add(discord.AudioSource())
+        self.player.set_queue(self.queue)
+
         self.command.player = self.player
+
+        await self.ctx._connect_author()
+        await self.ctx._connect()
+
+        self.player.play(self.ctx.voice_client)
     
-    async def test(self):
+    async def test_normal(self):
         await self.command.execute(self.ctx)
 
         self.assertTrue(self.player._calledNext)
+        self.assertEqual(self.ctx.sent, 'Ça marche [author.mention]')
+
+    async def test_author_not_connected(self):
+        await self.ctx._disconnect_author()
+
+        await self.command.execute(self.ctx)
+
+        self.assertFalse(self.player._calledNext)
+        self.assertEqual(self.ctx.sent, 'Tu dois être connecté à un salon audio pour ça [author.mention]')
+
+    async def test_not_connected(self):
+        await self.ctx._disconnect()
+
+        await self.command.execute(self.ctx)
+
+        self.assertFalse(self.player._calledNext)
+        self.assertEqual(self.ctx.sent, 'Je suis pas connecté au salon [author.mention]')
+
+    async def test_not_playing(self):
+        self.ctx.voice_client.stop()
+
+        await self.command.execute(self.ctx)
+
+        self.assertFalse(self.player._calledNext)
+        self.assertEqual(self.ctx.sent, 'Je joue rien pour l\'instant [author.mention]')
 
