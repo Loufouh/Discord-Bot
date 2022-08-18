2f638a8015ecd420dd447d17bb384ccc219ff2cc
Louis700 <louis.masson.contact@gmail.com>
2022-08-18 18:06:50 +0200

    :recycle: Refactor test.dummies.audio_url_retriever

--- test/dummies/audio_url_retriever/test_default.py
+++ test/dummies/audio_url_retriever/test_default.py
@@ -3,9 +3,10 @@ import unittest
 from dummies.audio_url_retriever import AudioUrlRetriever_dummy
 
 class TestAudioUrlRetriever_default(unittest.TestCase):
+    def setUp(self):
+        self.retriever = AudioUrlRetriever_dummy()
+
     def test(self):
-        retriever = AudioUrlRetriever_dummy()
-        
-        self.assertEqual(retriever._retrievedLink, '')
-        self.assertFalse(retriever._triggerWrongUrlException)
+        self.assertEqual(self.retriever._retrievedLink, '')
+        self.assertFalse(self.retriever._triggerWrongUrlException)
 
