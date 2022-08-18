59a587dbea63d6d53d49e347c3512143d8dd7afc
Louis700 <louis.masson.contact@gmail.com>
2022-08-18 06:04:21 +0000

    :recycle: Refactor commands.objects.pause

--- commands/objects/pause.py
+++ commands/objects/pause.py
@@ -14,15 +14,18 @@ class PauseCommand:
             await self.messageSender.send('Tu dois être connecté au salon pour ça')
         except AlreadyPausedException:
             await self.messageSender.send('C\'est déjà en pause')
-    
+
     async def try_to_execute(self, ctx):
+        self.verify_context(ctx)
+
+        ctx.voice_client.pause()
+        await self.messageSender.send('Je mets en pause')
+
+    def verify_context(self, ctx):
         if ctx.author.voice is None:
             raise AuthorNotConnectedException()
         if ctx.voice_client is None:
             raise NotConnectedException()
         if ctx.voice_client.is_paused():
             raise AlreadyPausedException()
-
-        ctx.voice_client.pause()
-        await self.messageSender.send('Je mets en pause')
 
