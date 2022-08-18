ca4f8d008449928240627a38aeb2faadeb13000b
Louis700 <louis.masson.contact@gmail.com>
2022-08-18 18:40:17 +0200

    :recycle: refactor test.music.audio_source_generator

--- test/music/audio_source_generator/test_default.py
+++ test/music/audio_source_generator/test_default.py
@@ -4,8 +4,9 @@ from music.audio_source_generator.common import AudioSourceGenerator
 from music.audio_url_retriever import AudioUrlRetriever
 
 class TestAudioSourceGenerator_default(unittest.TestCase):
-    def test(self):
-        generator = AudioSourceGenerator()
+    def setUp(self):
+        self.generator = AudioSourceGenerator()
 
-        self.assertIsInstance(generator.urlRetriever, AudioUrlRetriever)
+    def test(self):
+        self.assertIsInstance(self.generator.urlRetriever, AudioUrlRetriever)
 
